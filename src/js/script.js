4e20d7b14b0d918d65f98000dd00cdb30051b1ef
Mario S <mario@rsk.co>
2016-10-04 18:09:33 -0300

    Colours

--- src/js/script.js
+++ src/js/script.js
@@ -13,10 +13,10 @@
 	$.fn.sparkline.defaults.bar.tooltipFormat = $.spformat('<div class="tooltip-arrow"></div><div class="tooltip-inner">{{prefix}}{{value}} {{suffix}}</div>');
 	$.fn.sparkline.defaults.bar.colorMap = $.range_map({
 		'0:6': '#10a0de',
-		'6:15': '#7bcc3a',
-		'15:40': '#FFD162',
-		'40:60': '#ff8a00',
-		'60:': '#F74B4B'
+		'6:35': '#7bcc3a',
+		'35:50': '#FFD162',
+		'50:70': '#ff8a00',
+		'70:': '#F74B4B'
 	});
 
 	moment.relativeTimeThreshold('s', 60);
